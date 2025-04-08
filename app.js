d2324b7022bfec3d99520a3f2b35b731d93a4a3c
catatankuliah-lab <nanasupriatnakursus@gmail.com>
2025-04-08 16:09:35 +0700

    fix gitignore buat folder uploads

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const upload = multer(); // Inisialisasi multer
 
 app.use(
     cors({
-        origin: "https://apikasma.delapandelapanlogistics.com",
+        origin: "https://apikasma.delapandelapanlogisticss.com",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
