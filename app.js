8c22a7eda4ee6d1b181126bb586f0285092efbe8
aldyrizkyjuniar <juniaraldyrizky@gmail.com>
2025-04-08 17:14:04 +0700

    link fix

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const upload = multer(); // Inisialisasi multer
 
 app.use(
     cors({
-        origin: "https://apikasma.delapandelapanlogistics2.com",
+        origin: "https://apikasma.delapandelapanlogistics.com",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
