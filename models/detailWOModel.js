490073b3070d552540fc3a03bd2dca5c0d671f21
Nana Supriatna <pineapple@Nanas-MacBook-Air.local>
2025-07-01 06:25:22 +0700

    Commit otomatis pada Tuesday 2025-07-01 06:25:22

--- models/detailWOModel.js
+++ models/detailWOModel.js
@@ -21,7 +21,14 @@ const DetailWO = {
   },
 
   getDetailWOByIdWO: async (id_wo) => {
-    const query = `SELECT * FROM detail_wo WHERE id_wo = ?`;
+    const query = `
+    SELECT detail_wo.*, provinsi.nama_provinsi, kabupaten_kota.nama_kabupaten_kota, kecamatan.nama_kecamatan, desa_kelurahan.nama_desa_kelurahan
+    FROM detail_wo
+    JOIN provinsi ON provinsi.kode_provinsi = detail_wo.kode_provinsi
+    JOIN kabupaten_kota ON kabupaten_kota.kode_kabupaten_kota = detail_wo.kode_kabupaten_kota
+    JOIN kecamatan ON kecamatan.kode_kecamatan = detail_wo.kode_kecamatan
+    JOIN desa_kelurahan ON desa_kelurahan.kode_desa_kelurahan = detail_wo.kode_desa_kelurahan
+    WHERE detail_wo.id_wo = ?`;
 
     const result = await sequelize.query(query, {
       replacements: [id_wo],
